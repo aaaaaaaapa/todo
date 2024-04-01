5ce4d62e07726dc6ccf6c5b29d750afbd0fac472
aaaaaaaapa <dsaurb@yandex.ru>
2024-04-02 01:27:41 +0300

    js done

--- main.js
+++ main.js
@@ -1,15 +1,42 @@
-const taskForm = document.querySelector('.task-form');
-const taskList = document.querySelector('.task-list');
-
-
-const createElement = (elem, className, text='') => {
+const createElement = (elem, className, text='', placeholder='') => {
     const element = document.createElement(elem);
     element.classList.add(className);
     element.textContent = text;
+    if (elem === 'input') {
+        element.placeholder = placeholder;
+    }
     return element;
 }
 
-const createAndAppendToListItem = (text) => {
+const createTitle = (text='Список дел') => {
+    return createElement('h1', 'header-text', text);
+}
+
+const createForm = (placeholder='Введите текст...', text='Добавить') => {
+    const form = createElement('form', 'task-form');
+    const input = createElement('input', 'title-input', '', placeholder);
+    const btn = createElement('button', 'add-btn', text);
+
+    form.append(input, btn);
+
+    return {
+        form,
+        input,
+        btn
+    };
+}
+
+const createList = () => {
+    const divList = createElement('div', 'tasks');
+    const ul = createElement('ul', 'task-list');
+    divList.append(ul);
+    return {
+        divList,
+        ul
+    }
+}
+
+const createListItem = (text) => {
 
     const li = createElement('li', 'task-item');
 
@@ -29,29 +56,27 @@ const createAndAppendToListItem = (text) => {
 }
 
 const addToList = (form) => {
-
     const value = form.elements[0].value;
 
     if (value.trim() === '') {
         alert('Введите текст');
     }
     else {
-        const li = createAndAppendToListItem(value);
-        taskList.append(li);
+        const ul = document.querySelector('.task-list');
+        const li = createListItem(value);
+        ul.append(li);
     }
-
 }
 
 const handleFormSubmit = (event) => {
     event.preventDefault();
-    addToList(event.target)
-    event.target.reset()
+    addToList(event.target);
+    event.target.reset();
 }
 
 const handleBtnClick = (event) => {
     const tg = event.target;
     const li = tg.closest('li');
-    console.log(taskList.children[li])
 
     if (tg.classList.contains('status-btn') || tg === li) {
         li.classList.toggle('active');
@@ -61,11 +86,16 @@ const handleBtnClick = (event) => {
     }
 }
 
-taskForm.addEventListener('submit', handleFormSubmit);
-taskForm.addEventListener('keydown', function(e) {
-    if (e.key === 13) {
-        handleFormSubmit()
-    }
-});
 
-taskList.addEventListener('click', handleBtnClick);
+document.addEventListener('DOMContentLoaded', () => {
+    const formCont = document.querySelector('.form-container');
+
+    const h1 = createTitle();
+    const {form} = createForm(); 
+    const {divList, ul} = createList();
+
+    formCont.append(h1, form, divList);
+
+    form.addEventListener('submit', handleFormSubmit); 
+    ul.addEventListener('click', handleBtnClick);
+});
